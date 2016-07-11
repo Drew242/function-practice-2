d423247626976639876e2d7cd9ec95e6fdce4d53
Drew Conly <atconly@gmail.com>
2016-07-10 18:16:18 -0600

    sweet twins function

--- main.js
+++ main.js
@@ -40,6 +40,51 @@ function removeM(string) {
   console.log(newString.join(''));
 }
 
+function printObject(obj) {
+  for(var key in obj) {
+    console.log(key + " is " + obj[key]);
+  }
+}
+
+function vowels(string1) {
+  var newArray = [];
+  var array = string1.split('');
+  for (i = 0; i < array.length; i++) {
+    if (array[i] === 'a' || array[i] === 'e' || array[i] === 'i' || array[i] === 'o' || array[i] === 'u') {
+      newArray.push(array[i]);
+    }
+  }
+  console.log(newArray);
+}
+
+function twins(arr) {
+  if ((arr.length % 2) != 0) {
+    console.log(false);
+    return false;
+  }
+  while (arr.length > 0) {
+    if (arr[0] === arr[1]) {
+      arr.shift();
+      arr.shift();
+    } else {
+      console.log(false);
+      return false;
+      break;
+    }
+  }
+  console.log(true);
+  return true; 
+}
+
+
+function or() {
+
+}
+
+function unique() {
+
+}
+
 
 var thing = ({name: "Jack", age: 24})
 getName(thing);
@@ -53,3 +98,13 @@ var num = -2;
 negativeIndex(arr, num);
 var string = "McNamara";
 removeM(string);
+var obj = { a: 10, b: 20, c: 30 };
+printObject(obj);
+var string1 = 'What evil odd ducks!';
+vowels(string1);
+var array1 = [1, 1, 2, 2, 3, 3];
+var array2 = ['a', 'a', 'a'];
+var array3 = ['a', 'b', 'b', 'b'];
+twins(array1);
+twins(array2);
+twins(array3);
